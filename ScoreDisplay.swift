8b67348d2ffd31f3e97ac07cacd8bc2ce0ea7f56
Xiaoyu Guo <xg229@cornell.edu>
2016-03-22 03:51:12 -0400

    bug fixed, remove subviews of view every time display numbers

--- ScoreDisplay.swift
+++ ScoreDisplay.swift
@@ -61,8 +61,18 @@ class ScoreDisplay: NSObject {
         return count
     }
     
+    // remove subviews from a UIView
+    func removeAllSubviewsFrom(view : UIView) {
+        for subview in view.subviews {
+            if subview .isKindOfClass(UIImageView) {
+                subview.removeFromSuperview()
+            }
+        }
+    }
+    
     // display any score you want
     func displayAnyScore(score : Int) {
+        removeAllSubviewsFrom(displayView)
         processScore(score)
         processFrames(score)
         let count = getNumOfDigits(score)
@@ -108,7 +118,7 @@ class ScoreDisplay: NSObject {
                 frames.append(frame)
             }
         }
-        // if count is even number
+            // if count is even number
         else {
             let halfCount = count / 2
             // before
